97b4a45428745d737e69c032f5d35e77dd9bb2fd
bbastanza <b.bastanza@gmail.com>
2020-09-10 10:02:35 -0700

    commit refactor fetch

--- src/dataComponents/TableData.js
+++ src/dataComponents/TableData.js
@@ -17,28 +17,51 @@ function TableData(props) {
         if (!isLoading) createTableRows();
     }, [isLoading]);
 
+    const formatHeight = character => {
+        let feet = Math.floor((character.height * 0.3937008) / 12);
+        let inches = Math.round((character.height * 0.3937008) % 12);
+        if (inches === 12) {
+            feet += 1;
+            inches = 0;
+        }
+        return isNaN(feet) && isNaN(inches) ? "unknown" : `${feet}' ${inches}"`;
+    };
+
+    const formatWeight = character => {
+        return isNaN(Math.floor(character.mass * 2.204623)) ? "unknown" : Math.floor(character.mass * 2.204623);
+    };
+
+    const fetchHomeworld = character => {
+        return axios
+            .get(character.homeworld)
+            .then(response => response.data.name)
+            .catch(error => console.log(error));
+    };
+
+    const fetchSpecies = character => {
+        let species = "Human";
+        if (character.species.length > 0) {
+            species = axios
+                .get(character.species[0])
+                .then(response => response.data.name)
+                .catch(error => console.log(error));
+        }
+        return species;
+    };
+
     const fetchData = async () => {
         const returnedCharacters = await axios
             .get(`https://swapi.dev/api/people/?page=${props.pageNumber}`)
             .then(response => response.data.results)
             .catch(error => console.log(error));
         for (const character of returnedCharacters) {
-            character.homeworldName = await axios
-                .get(character.homeworld)
-                .then(response => response.data.name)
-                .catch(error => console.log(error));
-            let species = "Human";
-            if (character.species.length > 0) {
-                species = await axios
-                    .get(character.species[0])
-                    .then(response => response.data.name)
-                    .catch(error => console.log(error));
-            }
-            character.speciesName = species;
+            character.weight = formatWeight(character);
+            character.heightFormatted = formatHeight(character);
             character.id = Math.random();
+            character.speciesName = await fetchSpecies(character);
+            character.homeworldName = await fetchHomeworld(character);
         }
         setCharacters([...returnedCharacters]);
-
         setIsLoading(false);
     };
 
--- src/dummyComponents/TableDataRow.js
+++ src/dummyComponents/TableDataRow.js
@@ -1,24 +1,12 @@
 import React from "react";
 
 function TableDataRow(props) {
-    const weight = isNaN(Math.floor(props.character.mass * 2.204623))
-        ? "unknown"
-        : Math.floor(props.character.mass * 2.204623);
-
-    let feet = Math.floor((props.character.height * 0.3937008) / 12);
-    let inches = Math.round((props.character.height * 0.3937008) % 12);
-    if (inches === 12) {
-        feet += 1;
-        inches = 0;
-    }
-    const height = isNaN(feet) && isNaN(inches) ? "unknown" : `${feet}' ${inches}"`;
-
     return (
         <tr className="altFont">
             <td>{props.character.name}</td>
             <td>{props.character.birth_year}</td>
-            <td>{height}</td>
-            <td>{weight}</td>
+            <td>{props.character.heightFormatted}</td>
+            <td>{props.character.weight}</td>
             <td>{props.character.homeworldName}</td>
             <td>{props.character.speciesName}</td>
         </tr>
